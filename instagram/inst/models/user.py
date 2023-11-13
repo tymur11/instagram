4d5d7aff5fb95a07ad069572cd28ab57e0e034e0
tymur <qwe@gmail.com>
2023-11-12 22:55:56 -0600

    created project

---
+++ instagram/inst/models/user.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models 
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='media/avatar/', default='media/avatar/default.jpg')
+
+
